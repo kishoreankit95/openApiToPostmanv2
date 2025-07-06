494fadfbbd10cb52e9d0330514e87cb2c4608b5e
Ankit Kishore <kishore.ankit95@gmail.com>
2025-07-06 21:57:39 +0530

    adding frontend files

---
+++ UI/swaggerpostmanui/src/Converter.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Converter = () => {
+
+    const [file, setFile] = useState(null);
+    const [downloadLink, setDownloadLink] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    }
+
+    const handleFileUpload = async () => {
+        if(!file) return;
+        const formData = new FormData();
+        formData.append('file', file);
+        try{
+            const response = await axios.post('http://localhost:3001/convert', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                responseType: 'blob'
+            });
+
+            const blob = new Blob([response.data]);
+            const url = window.URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = url;
+
+            link.setAttribute('download', 'postman-collection.json');
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+
+            window.URL.revokeObjectURL(url);
+        }
+        catch(error){
+            console.error('Error uploading file:', error);
+        }
+    }
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange}/>
+            <button onClick={handleFileUpload}>Convert</button>
+        </div>
+  )
+}
+
+export default Converter
